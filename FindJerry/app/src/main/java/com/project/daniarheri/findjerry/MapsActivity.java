aba62d9915b1513bdbfe4e6faa67673b1773639d
daniarherikurniawan <ddhhkk2@gmail.com>
2015-03-03 23:45:31 +0700

    Positioning done

--- FindJerry/app/src/main/java/com/project/daniarheri/findjerry/MapsActivity.java
+++ FindJerry/app/src/main/java/com/project/daniarheri/findjerry/MapsActivity.java
@@ -3,25 +3,66 @@ package com.project.daniarheri.findjerry;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MapsActivity extends FragmentActivity implements SensorEventListener {
 
@@ -38,6 +79,8 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
     private float[] mOrientation = new float[3];
     private float mCurrentDegree = 0f;
     static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+    MarkerOptions mapMarkerMe = new MarkerOptions();
+    MarkerOptions mapMarkerJerry = new MarkerOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +105,8 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
         mSensorManager.unregisterListener((android.hardware.SensorEventListener) this, mMagnetometer);
     }
 
-    private void prepareCompass(){
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+    private void prepareCompass() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mPointer = (ImageView) findViewById(R.id.pointer);
@@ -94,7 +137,7 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
             SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
             SensorManager.getOrientation(mR, mOrientation);
             float azimuthInRadians = mOrientation[0];
-            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+            float azimuthInDegress = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
             RotateAnimation ra = new RotateAnimation(
                     mCurrentDegree,
                     -azimuthInDegress,
@@ -116,15 +159,60 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
 
     }
 
+    private void updateMap(float lat, float lon) {
+        mMap.addMarker(mapMarkerJerry.position(new LatLng(lat, lon)).title("Jerry's Position")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.markerjerry)));
+
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(lat, lon));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(20);
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+    }
+
+
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        double lat = -6.890608 , lon = 107.609861 ;
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(lat, lon));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+
+        mMap.addMarker(mapMarkerMe.position(new LatLng(lat,lon)).title("My Position")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.markerme)));
     }
 
     public void exit(View view) {
         System.exit(0);
     }
 
-    public void scanQR(View v){
+
+    public void findJerry(View view) throws IOException, ExecutionException, InterruptedException {
+        RequestTask RT = new RequestTask();
+        RT.execute("http://167.205.32.46/pbd/api/track?nim=13512064%20(167.205.32.46/pbd/api/track?nim=13512064");
+        String responseString = RT.get();
+        extractResponseString(responseString);
+
+    }
+
+    private void extractResponseString(String jsonStr) {
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                String lantitude = jsonObj.getString("lat");
+                String longitude = jsonObj.getString("lon");
+                String validTime = jsonObj.getString("valid_until");
+                Toast toast = Toast.makeText(this, lantitude + "  " + longitude, Toast.LENGTH_LONG);
+                toast.show();
+                updateMap(Float.parseFloat(lantitude), Float.parseFloat(longitude));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void scanQR(View v) {
         try {
             //start the scanning activity from the com.google.zxing.client.android.SCAN intent
             Intent intent = new Intent(ACTION_SCAN);
@@ -170,5 +258,6 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
             }
         }
     }
+
 
 }
