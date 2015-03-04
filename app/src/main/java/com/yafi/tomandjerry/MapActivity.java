cd209a1f8d7179cfe0794c930823b574800c7e94
Muhammad Yafi <yafithekid212@gmail.com>
2015-03-04 14:54:19 +0700

    Fixing layout

--- app/src/main/java/com/yafi/tomandjerry/MapActivity.java
+++ app/src/main/java/com/yafi/tomandjerry/MapActivity.java
@@ -3,19 +3,30 @@ package com.yafi.tomandjerry;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Image;
 import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,8 +34,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -36,7 +49,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MapActivity extends Activity {
+public class MapActivity extends FragmentActivity implements SensorEventListener{
     final String SERVER = "http://167.205.32.46/pbd";
     private GoogleMap map;
     private Marker marker;
@@ -46,11 +59,30 @@ public class MapActivity extends Activity {
     Timer timer;
     TimerTask autoUpdateTask;
 
+    private ImageView mPointer;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mPointer = (ImageView) findViewById(R.id.imageViewCompass);
+
+
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         //set default position
         marker = map.addMarker(new MarkerOptions()
                 .title("Target")
@@ -67,6 +99,15 @@ public class MapActivity extends Activity {
     protected void onResume(){
         super.onResume();
         startAutoUpdateTask();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
     }
 
     private void updateTargetLocation() {
@@ -92,6 +133,51 @@ public class MapActivity extends Activity {
         };
     }
 
+    public void moveCameraToTarget(View v){
+        map.animateCamera(CameraUpdateFactory.newLatLng(targetPosition));
+
+    }
+
+    public void moveCameraToMyLocation(View v){
+        double lat = map.getMyLocation().getLatitude();
+        double lng = map.getMyLocation().getLongitude();
+        map.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(lat,lng)));
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mAccelerometer) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor == mMagnetometer) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+            RotateAnimation ra = new RotateAnimation(
+                    mCurrentDegree,
+                    -azimuthInDegress,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
+
+            ra.setDuration(250);
+
+            ra.setFillAfter(true);
+
+            mPointer.startAnimation(ra);
+            mCurrentDegree = -azimuthInDegress;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
     private class UpdateTargetLocationTask extends AsyncTask<String,Void,String> {
 
         @Override
@@ -102,14 +188,16 @@ public class MapActivity extends Activity {
                 URL obj = new URL(url);
                 HttpURLConnection con = (HttpURLConnection) obj.openConnection();
                 con.setRequestMethod("GET");
-
+                con.setRequestProperty("Accept-Charset", "UTF-8");
                 con.connect();
                 int responseCode = con.getResponseCode();
 
                 InputStream is = con.getInputStream();
+                BufferedReader r = new BufferedReader(new InputStreamReader(is,"UTF8"));
                 StringBuffer sb = new StringBuffer();
-                while (is.available() > 0){
-                    sb.append(((char) is.read()));
+                int chr;
+                while ((chr = r.read()) != -1){
+                    sb.append(((char) chr));
                 }
                 return sb.toString();
             } catch (MalformedURLException mue){
@@ -121,12 +209,13 @@ public class MapActivity extends Activity {
 
         @Override
         protected void onPostExecute(String result) {
+            double lat = targetPosition.latitude;
+            double lng = targetPosition.longitude;
+            double oldLat = lat, oldLng = lng;
             try {
+                Log.d("test",result);
                 JSONObject jObject = new JSONObject(result);
                 Iterator<?> keys = jObject.keys();
-                double lat = targetPosition.latitude;
-                double lng = targetPosition.longitude;
-                double oldLat = lat, oldLng = lng;
                 while (keys.hasNext()) {
                     String key = (String) keys.next();
                     String value = jObject.getString(key);
@@ -143,8 +232,8 @@ public class MapActivity extends Activity {
                 marker.setPosition(targetPosition);
 //                Log.d("lat","target lattitude = " + Double.toString(targetPosition.latitude));
 //                Log.d("lng","target longitude = " + Double.toString(targetPosition.longitude));
-//                Log.d("lat","new lattitude = " + Double.toString(marker.getPosition().latitude));
-//                Log.d("lng","new longitude = " + Double.toString(marker.getPosition().longitude));
+                Log.d("lat","new lattitude = " + Double.toString(marker.getPosition().latitude));
+                Log.d("lng","new longitude = " + Double.toString(marker.getPosition().longitude));
                 if (Math.abs(lat - oldLat) <= 0.0001 && Math.abs(lng - oldLng) <= 0.0001){
                     //dont view toast
                 } else {
@@ -153,10 +242,15 @@ public class MapActivity extends Activity {
                 }
                 validUntil = (System.currentTimeMillis()/1000) + 10;
             } catch (JSONException je) {
-                Toast.makeText(getApplicationContext(), "JSONException: get json" + result, Toast.LENGTH_LONG).show();
+                je.printStackTrace();
+                Toast.makeText(getApplicationContext(), "JSONException:  " + je.getMessage(), Toast.LENGTH_LONG).show();
+                //set to default location
+                targetPosition = new LatLng(oldLat,oldLng);
             }
         }
     }
+
+
 
 }
 
