52aa5c5a20eef47ac168ecbf8c1f7992dcb73f00
Rikysamuel <rikz.samuel@gmail.com>
2015-03-06 20:13:24 +0700

    Google Maps

--- TomJerry/app/src/main/java/com/example/rikysamuel/tomjerry/fragmentStart.java
+++ TomJerry/app/src/main/java/com/example/rikysamuel/tomjerry/fragmentStart.java
@@ -1,6 +1,8 @@
 package com.example.rikysamuel.tomjerry;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -60,6 +62,32 @@ public class fragmentStart extends Fragment {
 
             TextView t = (TextView) getActivity().findViewById(R.id.textView);
             t.setText("long: " + longitude + " lat: " + latitude + " val: " + valid_until);
+            alert();
         }
+    }
+
+    public void alert(){
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Google Maps")
+                .setMessage("Do you want to see Jerry's Position on Map?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Google Maps",Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(getActivity(), MapsActivity.class);
+                        i.putExtra("long", longitude);
+                        i.putExtra("lat", latitude);
+                        i.putExtra("val", valid_until);
+                        startActivity(i);
+                        TextView t = (TextView) getActivity().findViewById(R.id.textView);
+                        t.setText("Google Maps......");
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
