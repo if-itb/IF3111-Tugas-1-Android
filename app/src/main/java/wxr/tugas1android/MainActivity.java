54ee9e3a5799c1345cee60450d39d395c3f1c598
felicia-christie <waxrain1217@gmail.com>
2015-03-06 17:39:26 +0700

    Basic functionality all done

--- app/src/main/java/wxr/tugas1android/MainActivity.java
+++ app/src/main/java/wxr/tugas1android/MainActivity.java
@@ -1,49 +1,188 @@
 package wxr.tugas1android;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.DataSetObserver;
-import android.os.Bundle;
+import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.PersistableBundle;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.Toast;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends ActionBarActivity {
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 
+
+    public double LatVal = -6, LongVal = 100;
+    public long theTime;
+    boolean lock = true;
+    public String time = "Undefined time";
+    public String contents = "Undefined content";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button MapsB = (Button) findViewById(R.id.button);
+        Button QRB = (Button) findViewById(R.id.button2);
+        Button Compass = (Button) findViewById(R.id.button3);
+        //lock = true;
+        //new Task().execute(getApplicationContext());
+        //while (lock){}
+
         MapsB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double LatVal = -6.890323, LongVal = 107.610381;
+
+                /*
+                JSONObject theData = getData("167.205.32.46/pbd/api/track?nim=13512039");
+                try {
+                    LatVal = theData.getDouble("lat");
+                    LongVal = theData.getDouble("long");
+                    theTime = theData.getLong("valid_until");
+                    time = String.format("%02d:%02d:%02d", (TimeUnit.MILLISECONDS.toHours(theTime) + 7),
+                            TimeUnit.MILLISECONDS.toMinutes(theTime) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(theTime)),
+                            TimeUnit.MILLISECONDS.toSeconds(theTime) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(theTime)));
+                }
+                catch(JSONException e){}
+                */
+
+
                 Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
                 intent.putExtra("Long", LongVal);
                 intent.putExtra("Lat", LatVal);
+                intent.putExtra("Valid", theTime);
+                intent.putExtra("ValidString", time);
                 startActivity(intent);
             }
         });
-        Button QRB = (Button) findViewById(R.id.button2);
+        Compass.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), compass.class);
+                startActivity(intent2);
+            }
+        });
+
+        QRB.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    //start the scanning activity from the com.google.zxing.client.android.SCAN intent
+                    Intent intent = new Intent(ACTION_SCAN);
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                    startActivityForResult(intent, 0);
+                } catch (ActivityNotFoundException anfe) {
+                    //on catch, show the download dialog
+                    showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+                }
+            }
+        });
 
     }
 
+    public class Task extends AsyncTask<Context, String, String> {
+        private Context context;
+        public JSONObject getData(String url) {
+            JSONObject theData = new JSONObject();
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
+            HttpResponse response;
+            String result = "";
+            try {
+                response = client.execute(request);
+                // Get the response
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF8"));
+
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result += line;
+                }
+                Log.d("api/track", result);
+
+                String resulte = result.substring(result.indexOf("{"), (result.lastIndexOf("}") + 1));
+                Log.d("api/track", resulte);
+                theData = new JSONObject(resulte);
+            } catch (Exception e) {
+            }
+
+            //Toast T = new Toast(getApplicationContext());
+            //T.setText(result);
+
+            //Toast.makeText(getApplication().getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            lock = false;
+            return theData;
+        }
+
+        @Override
+        protected String doInBackground(Context... params) {
+            context = params[0];
+            JSONObject theData = getData("http://167.205.32.46/pbd/api/track?nim=13512039");
+            try {
+                LatVal = theData.getDouble("lat");
+                LongVal = theData.getDouble("long");
+                theTime = theData.getLong("valid_until");
+
+                time = String.format("%02d:%02d:%02d", ((TimeUnit.SECONDS.toHours(theTime) + 7) % 24),
+                        TimeUnit.SECONDS.toMinutes(theTime) % 60,
+                        TimeUnit.SECONDS.toSeconds(theTime) % 60);
+            }
+            catch(JSONException e){}
+
+            return new String();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getApplication().getApplicationContext(), "Finished loading from PBD Server", Toast.LENGTH_LONG).show();
+        }
+    }
+    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return downloadDialog.show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,6 +204,70 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                //get the extras that are returned from the intent
+                contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                toast.show();
+
+                // send post_http
+                new postTask().execute(getApplicationContext());
+
+            }
+        }
+    }
+
+    public class postTask extends  AsyncTask<Context, String, String> {
+
+        public String restring = "Undefined restring";
+        @Override
+        protected String doInBackground(Context... params) {
+
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost("http://167.205.32.46/pbd/api/catch");
+            HttpResponse response;
+            post.addHeader("content-type", "application/json");
+            try {
+                restring = "";
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("nim", "13512039"));
+                nameValuePairs.add(new BasicNameValuePair("token", contents));
+
+                JSONObject jeson = new JSONObject();
+                jeson.put("nim", "13512039");
+                jeson.put("token", contents);
+
+                post.setEntity(new StringEntity(jeson.toString()));
+                response = client.execute(post);
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                Log.d("contents", contents);
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    restring += line;
+                }
+                Log.d("api/push", restring);
+
+            }
+            catch(Exception E){Log.d("reqBody", "paaah");}
+            //Toast.makeText(getApplication().getApplicationContext(), restring, Toast.LENGTH_LONG).show();
+
+
+            return new String();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getApplication().getApplicationContext(), restring, Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
 
--- app/src/main/java/wxr/tugas1android/MapsActivity.java
+++ app/src/main/java/wxr/tugas1android/MapsActivity.java
@@ -1,24 +1,53 @@
 package wxr.tugas1android;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
+    public double LatVal = -6, LongVal = 100;
+    public long theTime;
+    boolean lock = true;
+    public String time = "Undefined time";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        try {
+            new Task().execute(getApplicationContext()).get();
+            CountDown((1000 * theTime) - System.currentTimeMillis());
+            setUpMapIfNeeded();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -56,19 +85,113 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void setUpMap() {
+        /*
         double lat;
         double lang;
+        String thetime;
+        Long theTime;
+        */
         //TODO Get location data from another activity?
         Bundle B = getIntent().getExtras();
+
+        /*
         lat = B.getDouble("Lat");
         lang = B.getDouble("Long");
-/*
+        theTime = B.getLong("Valid");
+        thetime = B.getString("ValidString");
+        */
+    /*
         lat = -6.890323;
-        lang = 107.610381;*/
-        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lang)).title("Jerry"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lang), 15));
+        lang = 107.610381;
+        */
+        mMap.addMarker(new MarkerOptions().position(new LatLng(LatVal, LongVal)).title("Jerry"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(LatVal, LongVal), 18));
     }
 
+    private void CountDown(long theTimeDif){
+
+        new CountDownTimer(theTimeDif, 1000) {
+            private TextView mTextField = (TextView) findViewById(R.id.textView);
+
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mTextField.setText("Refreshing!");
+                try {
+                    new Task().execute(getApplicationContext()).get();
+                    setUpMap();
+                    CountDown((theTime * 1000) - System.currentTimeMillis());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
+
+    public class Task extends AsyncTask<Context, String, String> {
+        private Context context;
+        public JSONObject getData(String url) {
+            JSONObject theData = new JSONObject();
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
+            HttpResponse response;
+            String result = "";
+            try {
+                response = client.execute(request);
+                // Get the response
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF8"));
+
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result += line;
+                }
+                Log.d("api/track", result);
+
+                String resulte = result.substring(result.indexOf("{"), (result.lastIndexOf("}") + 1));
+                Log.d("api/track", resulte);
+                theData = new JSONObject(resulte);
+            } catch (Exception e) {
+            }
+
+            //Toast T = new Toast(getApplicationContext());
+            //T.setText(result);
+
+            //Toast.makeText(getApplication().getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            return theData;
+        }
+
+        @Override
+        protected String doInBackground(Context... params) {
+            context = params[0];
+            JSONObject theData = getData("http://167.205.32.46/pbd/api/track?nim=13512039");
+            try {
+                LatVal = theData.getDouble("lat");
+                LongVal = theData.getDouble("long");
+                theTime = theData.getLong("valid_until");
+
+                time = String.format("%02d:%02d:%02d", ((TimeUnit.SECONDS.toHours(theTime) + 7) % 24),
+                        TimeUnit.SECONDS.toMinutes(theTime) % 60,
+                        TimeUnit.SECONDS.toSeconds(theTime) % 60);
+
+            }
+            catch(JSONException e){}
+            lock = false;
+
+            return new String();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getApplication().getApplicationContext(), "Finished loading from PBD Server", Toast.LENGTH_LONG).show();
+
+        }
+    }
 
 
 }
