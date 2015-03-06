507c5e656b3e50b65d825e34206d954c386fe67e
henrymenori <henrymenori@yahoo.com>
2015-03-06 19:15:33 +0700

    Penambahan fungsi track dan catch

--- Tracker/app/src/main/java/henrymenori/tracker/MapsActivity.java
+++ Tracker/app/src/main/java/henrymenori/tracker/MapsActivity.java
@@ -10,6 +10,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,13 +21,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements SensorEventListener{
 
@@ -37,20 +55,29 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
     private SensorManager sensor; // device sensor
 
     TextView head;
+    TextView head2;
 
     // attribute for scan QR code
     static final String actionScan = "com.google.zxing.client.android.SCAN";
+
+    // attribute for tracker
+    private String content;
+    private long time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        //getRequest();
 
         // compass initialization
         img = (ImageView) findViewById(R.id.image);
         head = (TextView) findViewById(R.id.heading);
         sensor = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        head2 = (TextView) findViewById(R.id.text1);
+
+        setUpMapIfNeeded();
     }
 
     @Override
@@ -84,9 +111,10 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
     }
 
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        getRequest();
     }
 
+    // compass function
     @Override
     public void onSensorChanged(SensorEvent event) {
         float d = Math.round(event.values[0]);
@@ -111,6 +139,7 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
 
     }
 
+    // scanner function
     public void scanBar(View v) {
         try {
             Intent in = new Intent(actionScan);
@@ -144,7 +173,9 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
                 try {
                     a.startActivity(in);
                 }
-                catch(ActivityNotFoundException e) {}
+                catch(ActivityNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
         dialog.setNegativeButton(no, new DialogInterface.OnClickListener() {
@@ -156,11 +187,150 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
     public void onActivityResult(int request, int result, Intent in) {
         if(request == 0) {
             if(result == RESULT_OK) {
-                String content = in.getStringExtra("SCAN_RESULT");
+                content = in.getStringExtra("SCAN_RESULT");
                 String format = in.getStringExtra("SCAN_RESULT_FORMAT");
                 Toast t = Toast.makeText(this, "Content:" + content + " Format:" + format, Toast.LENGTH_LONG);
                 t.show();
+                postRequest();
             }
+        }
+    }
+
+    // tracker function
+    public void getRequest() {
+        GetTask gt = new GetTask();
+        gt.execute("http://167.205.32.46/pbd/api/track?nim=13512082");
+    }
+
+    private class GetTask extends AsyncTask<String, String, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(URI.create(params[0]));
+
+            HttpResponse response;
+            try {
+                response = client.execute(request);
+            }
+            catch (IOException e) {
+                response = null;
+                e.printStackTrace();
+            }
+
+            HttpEntity entity = null;
+            if(response != null) {
+                entity = response.getEntity();
+            }
+
+            JSONObject json = null;
+            try {
+                BufferedHttpEntity bufEntity = new BufferedHttpEntity(entity);
+                BufferedReader bufRead = new BufferedReader(new InputStreamReader(bufEntity.getContent()));
+
+                String s;
+                StringBuilder sb = new StringBuilder();
+                while((s = bufRead.readLine()) != null) {
+                    sb.append(s);
+                }
+                try {
+                    json = new JSONObject(sb.toString());
+                }
+                catch(JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+
+            if(json != null) {
+                content = json.toString();
+            }
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            super.onPostExecute(result);
+            try{
+                double lat = Double.parseDouble(result.getString("lat"));
+                double lng = Double.parseDouble(result.getString("long"));
+                time = Long.parseLong(result.getString("valid_until")) * 1000;
+                LatLng jerry = new LatLng(lat,lng);
+                new CountDownTimer((time - System.currentTimeMillis()), 1000) {
+                    public void onTick(long remainder) {
+                        head2.setText(String.valueOf((time - System.currentTimeMillis()) / 1000) + " seconds");
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        setUpMapIfNeeded();
+                    }
+                }.start();
+                Toast toast = Toast.makeText(MapsActivity.this,"Jerry Position ("+lat+","+lng+")",Toast.LENGTH_LONG);
+                toast.show();
+
+                // show marker
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(jerry).title("Marker"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jerry, 17));
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void postRequest(){
+        PostTask pt = new PostTask();
+        pt.execute();
+    }
+
+    private class PostTask extends AsyncTask<Void, String, String> {
+
+        @Override
+        protected String doInBackground(Void... params){
+            HttpClient client = new DefaultHttpClient();
+
+            String contents = null;
+            try{
+                HttpPost post = new HttpPost("http://167.205.32.46/pbd/api/catch");
+
+                JSONObject json = new JSONObject();
+                json.put("nim", "13512082");
+                json.put("token", content);
+
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,"application/json"));
+
+                post.setEntity(se);
+                HttpResponse response = client.execute(post);
+                if(response != null){
+                    HttpEntity entity = response.getEntity();
+                    BufferedHttpEntity buffEntity = new BufferedHttpEntity(entity);
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(buffEntity.getContent()));
+
+                    String line;
+                    StringBuilder sb = new StringBuilder();
+                    while((line = rd.readLine()) != null){
+                        sb.append(line);
+                    }
+                    contents = sb.toString();
+                }
+            }
+            catch (JSONException | IOException e) {
+                e.printStackTrace();
+            }
+
+            return contents;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            Toast toast = Toast.makeText(MapsActivity.this, result, Toast.LENGTH_LONG);
+            toast.show();
         }
     }
 }
