f4d5d0aaa13f18db244e6476a3f19417fe800adf
gifarikautsar <gifari13@gmail.com>
2015-03-06 16:39:15 +0700

    Sudah bisa menerima koordinat Jerry

--- Let'sCatchJerry/app/src/main/java/com/gifarikautsar/letscatchjerry/MapsActivity.java
+++ Let'sCatchJerry/app/src/main/java/com/gifarikautsar/letscatchjerry/MapsActivity.java
@@ -15,6 +15,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
@@ -32,6 +33,21 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 public class MapsActivity extends FragmentActivity implements LocationListener, SensorEventListener {
 
@@ -40,6 +56,9 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     private float currentDegree = 0f;
     private SensorManager mSensorManager;
     static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+    private LatLng Jerry;
+    private String contents ="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,9 +90,8 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
                 onLocationChanged(location);
             }
             locationManager.requestLocationUpdates(provider, 20000, 0, this);
-
-
         }
+        getRequest();
     }
 
     @Override
@@ -186,6 +204,78 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
                 Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
                 toast.show();
             }
+        }
+    }
+
+    private void setUpMap() {
+        googleMap.clear();
+        googleMap.addMarker(new MarkerOptions().position(Jerry).title("Catch Me! :3"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Jerry));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(Jerry)      // Sets the center of the map to LatLng (refer to previous snippet)
+                .zoom(18)                   // Sets the zoom
+                .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    public void getRequest(){
+        GetTask GT = new GetTask();
+        GT.execute("http://167.205.32.46/pbd/api/track?nim=13512020");
+    }
+
+    private class GetTask extends AsyncTask<String, String, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... uri) {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(URI.create(uri[0]));
+            HttpEntity entity = null;
+            JSONObject json = null;
+            StringBuffer sb = new StringBuffer();
+            String line = "";
+            try{
+                HttpResponse response = (HttpResponse) client.execute(request);
+                entity = response.getEntity();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            try{
+                BufferedHttpEntity buffEntity = new BufferedHttpEntity(entity);
+                BufferedReader rd = new BufferedReader(new InputStreamReader(buffEntity.getContent()));
+                while((line = rd.readLine()) != null){
+                    sb.append(line);
+                }
+                try{
+                    json = new JSONObject(sb.toString());
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            contents = json.toString();
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            super.onPostExecute(result);
+            double lat;
+            double lng;
+            try{
+                lat = Double.parseDouble(result.getString("lat"));
+                lng = Double.parseDouble(result.getString("long"));
+                Jerry = new LatLng(lat,lng);
+                Toast toast = Toast.makeText(MapsActivity.this,"Jerry Position ("+lat+","+lng+")",Toast.LENGTH_LONG);
+                toast.show();
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+            setUpMap();
         }
     }
 }
