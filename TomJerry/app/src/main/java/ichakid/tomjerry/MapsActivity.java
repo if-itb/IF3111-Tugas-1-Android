da11e236afa3978bd1b6161f636e0328f0ca89ca
ichakid <choirunnisa.fatima@gmail.com>
2015-03-07 22:39:22 +0700

    Final

--- TomJerry/app/src/main/java/ichakid/tomjerry/MapsActivity.java
+++ TomJerry/app/src/main/java/ichakid/tomjerry/MapsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Point;
@@ -20,7 +21,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -48,6 +48,7 @@ import android.os.Handler;
 public class MapsActivity extends FragmentActivity implements LocationListener, SensorEventListener {
     private LatLng myPosition;
     private LatLng JERRY;                   //lokasi Jerry
+    private Marker me;
     private Marker jerry;
     private GoogleMap mMap;
     private ImageView imgCompass;           //Gambar compass
@@ -71,31 +72,31 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         try {
-            setUpMapIfNeeded();
+            setUpMap();
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        //Memindahkan kamera langsung ke jerry dengan zoom 15
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(JERRY, 15));
+        //Memindahkan kamera langsung ke myPosition dengan zoom 15
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 17));
         //Zoom in, animasi kameranya
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
 
         imgCompass = (ImageView) findViewById(R.id.imageViewCompass);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         textCounter = (TextView) findViewById(R.id.textCounter);
-        textCounter.setText(String.valueOf(time));
+        textCounter.setText("Jerry will be gone in " + String.valueOf(time));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         try {
-            setUpMapIfNeeded();
+            setUpMap();
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -110,93 +111,6 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         super.onPause();
         mSensorManager.unregisterListener(this, accelerometer);
         mSensorManager.unregisterListener(this, magnetometer);
-    }
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() throws ExecutionException, InterruptedException {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() throws ExecutionException, InterruptedException {
-        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mMap = fm.getMap();
-        mMap.setMyLocationEnabled(true);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
-        Location location = locationManager.getLastKnownLocation(provider);
-        myPosition = new LatLng(location.getLatitude(), location.getLongitude());
-        if(location!=null){
-            onLocationChanged(location);
-        }
-        locationManager.requestLocationUpdates(provider, 20000, 0, this);
-        getJerryPosition();
-        jerry = mMap.addMarker(new MarkerOptions()
-                .position(JERRY)
-                .title("Jerry")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.jerry)));
-    }
-
-    public void getJerryPosition() throws ExecutionException, InterruptedException {
-        RequestTask req = new RequestTask();
-        req.execute("http://167.205.32.46/pbd/api/track?nim=13512084");
-        String response = (String) req.get();
-        try {
-            JSONObject json = new JSONObject(response);
-            String lat = json.getString("lat");
-            String lon = json.getString("long");
-            String valid_until = json.getString("valid_until");
-            time = Long.parseLong(valid_until) * 1000;
-            JERRY = new LatLng(Float.parseFloat(lat), Float.parseFloat(lon));
-            new CountDownTimer((time - System.currentTimeMillis()), 1000){
-                public void onTick(long millisUntilFinished){
-                    long cur = (time - System.currentTimeMillis())/1000;
-                    textCounter.setText(cur / 3600 + ":" + cur % 3600 / 60 + ":" + cur % 3600 % 60 );
-                }
-                public void onFinish() {
-                    time = 0;
-                    try {
-                        getJerryPosition();
-                        animateMarker(JERRY);
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
-        } catch (JSONException e){
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -228,6 +142,103 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView tvLocation = (TextView) findViewById(R.id.tv_location);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        myPosition = new LatLng(latitude, longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        tvLocation.setText("Latitude:" +  latitude  + ", Longitude:"+ longitude );
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() throws ExecutionException, InterruptedException {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                mMap = fm.getMap();
+                mMap.getUiSettings().setCompassEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                Criteria criteria = new Criteria();
+                String provider = locationManager.getBestProvider(criteria, true);
+                Location location = locationManager.getLastKnownLocation(provider);
+                if (location != null) {
+                    myPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                    onLocationChanged(location);
+                }
+                locationManager.requestLocationUpdates(provider, 20000, 0, this);
+                me = mMap.addMarker(new MarkerOptions()
+                        .position(myPosition)
+                        .title("Me")
+                        .icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.tom)));
+                getJerryPosition();
+                jerry = mMap.addMarker(new MarkerOptions()
+                        .position(JERRY)
+                        .title("Jerry")
+                        .icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.jerry)));
+            }
+        }
+    }
+
+    public void getJerryPosition() throws ExecutionException, InterruptedException {
+        RequestTask req = new RequestTask();
+        req.execute("http://167.205.32.46/pbd/api/track?nim=13512084");
+        String response = (String) req.get();
+        try {
+            JSONObject json = new JSONObject(response);
+            String lat = json.getString("lat");
+            String lon = json.getString("long");
+            String valid_until = json.getString("valid_until");
+            time = Long.parseLong(valid_until) * 1000;
+            JERRY = new LatLng(Float.parseFloat(lat), Float.parseFloat(lon));
+            new CountDownTimer((time - System.currentTimeMillis()), 1000){
+                public void onTick(long millisUntilFinished){
+                    long cur = (time - System.currentTimeMillis())/1000;
+                    textCounter.setText("Jerry will be gone in " + cur / 3600 + ":" + cur % 3600 / 60 + ":" + cur % 3600 % 60 );
+                }
+                public void onFinish() {
+                    time = 0;
+                    try {
+                        getJerryPosition();
+                        animateMarker(JERRY);
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
 
     public void scanQR(View v){
         try {
@@ -263,28 +274,66 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
-                String contents = intent.getStringExtra("SCAN_RESULT");
+                final String contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
-                toast.show();
-                try {
-                    makePostRequest(contents);
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                final Context context = this;
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("Get Token");
+                alertDialogBuilder
+                        .setMessage("Token: " + contents)
+                        .setCancelable(false)
+                        .setPositiveButton("Report to Spike",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                try {
+                                    makePostRequest(contents);
+                                } catch (ExecutionException e) {
+                                    e.printStackTrace();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("Exit to Map",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
             }
         }
     }
 
-    public void makePostRequest(String token) throws ExecutionException, InterruptedException {
+    public void makePostRequest(String token) throws ExecutionException, InterruptedException, JSONException {
         PostTask pt = new PostTask();
         pt.execute("http://167.205.32.46/pbd/api/catch", token);
         String response = pt.get().toString();
-        Log.d("Http Post Response:", response);
-        Toast toast = Toast.makeText(this, "Http Post Response:" + response, Toast.LENGTH_LONG);
-        toast.show();
+        JSONObject json = new JSONObject(response);
+        int status = json.getInt("code");
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        String title = "";
+        String message = json.getString("message");
+        switch(status){
+            case 200 : title = "Congratulations!"; message += "\nYou are awesome."; break;
+            case 400 : title = "Oh no!"; message += "\nSorry, you should try again."; break;
+            case 403 : title = "Oh no!"; message += "\nSorry, you should try again."; break;
+        }
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        if(!this.isFinishing()){
+            alertDialog.show();
+        }
     }
 
     public void animateMarker(final LatLng toPosition) {
@@ -316,26 +365,13 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         });
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        TextView tvLocation = (TextView) findViewById(R.id.tv_location);
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        myPosition = new LatLng(latitude, longitude);
+    public void viewOnMe(View v){
         mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-        tvLocation.setText("Latitude:" +  latitude  + ", Longitude:"+ longitude );
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
+    public void viewOnJerry(View v){
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(JERRY));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
     }
 }
