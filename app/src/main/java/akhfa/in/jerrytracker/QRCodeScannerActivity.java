2c296a7b49cd2338d88a25186277eeff0467b72c
akhfa <akhmadfakhoni@gmail.com>
2015-03-07 10:26:14 +0700

    Added qrcode scanner activity

---
+++ app/src/main/java/akhfa/in/jerrytracker/QRCodeScannerActivity.java
@@ -0,0 +1,55 @@
+package akhfa.in.jerrytracker;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class QRCodeScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    public void scanQR(View v)
+    {
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v("TAG", rawResult.getText()); // Prints scan results
+        Log.v("TAG", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+        Toast toast = Toast.makeText(this, "Content:" + rawResult.getText() + " Format:" + rawResult.getBarcodeFormat().toString(), Toast.LENGTH_LONG);
+        toast.show();
+        mScannerView.stopCamera();
+        //setContentView(R.layout.activity_main);
+    }
+}
