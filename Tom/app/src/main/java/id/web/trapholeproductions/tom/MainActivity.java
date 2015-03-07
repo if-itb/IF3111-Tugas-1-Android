f186bc5bf2bee363c174cd1026c0b20e277d8af6
Trapsilo P. Bumi <traphole@gmail.com>
2015-03-07 13:33:29 +0700

    All features completed.

--- Tom/app/src/main/java/id/web/trapholeproductions/tom/MainActivity.java
+++ Tom/app/src/main/java/id/web/trapholeproductions/tom/MainActivity.java
@@ -1,5 +1,12 @@
 package id.web.trapholeproductions.tom;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,45 +14,115 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, SensorEventListener {
 
     Handler mHandler = new Handler();
+
+    double latitude = Double.NaN, longitude = Double.NaN;
     long timeLeft = -1;
-    boolean stopThread = true;
+    RequestQueue queue;
+    int catchResponseCode;
 
-    private final int STOP_THREAD = 1;
+    private ImageView image;
+    private float currentDegree = 0f;
+    private SensorManager mSensorManager;
+
     private final String DEBUG_TAG = "Tom&JerryDebug";
+    private final String TRACK_URL = "http://167.205.32.46/pbd/api/track?nim=13510052";
+    private final String CATCH_URL = "http://167.205.32.46/pbd/api/catch";
+    private final LatLng POSISI_KOLAM_INTEL = new LatLng(-6.890323, 107.610381);
 
-    public void askSpike(View view) {
+    Runnable rticker = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                Log.d(DEBUG_TAG, "ticker run");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                if (timeLeft > -1) {
+                    timeLeft--;
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final TextView time_text = (TextView) findViewById(R.id.time_text);
+                            time_text.setText(timeLeftText());
+                        }
+                    });
+                } else {
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final TextView time_text = (TextView) findViewById(R.id.time_text);
+                            time_text.setText("No time left.");
+                        }
+                    });
+                }
+                if (Thread.interrupted()) return;
+            }
+        }
+    };
+    Thread ticker;
 
-// Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://167.205.32.46/pbd/api/track?nim=13510052";
+    Runnable rpoller = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                Log.d(DEBUG_TAG, "poller run");
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        final TextView lat_text = (TextView) findViewById(R.id.lat_text);
+                        final TextView long_text = (TextView) findViewById(R.id.long_text);
+                        lat_text.setText("Updating...");
+                        long_text.setText("Updating...");
+                        askSpike();
+                    }
+                });
+                if (Thread.interrupted()) return;
+            }
+        }
+    };
+    Thread poller;
 
-// Request a string response from the provided URL.
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(url, null,
+    public void askSpike() {
+        queue.add(new JsonObjectRequest(TRACK_URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -54,100 +131,191 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
                         final TextView time_text = (TextView) findViewById(R.id.time_text);
                         MapFragment mapFragment = (MapFragment) getFragmentManager()
                                 .findFragmentById(R.id.map);
-                        GoogleMap map = mapFragment.getMap();
+                        if (mapFragment != null) {
+                            GoogleMap map = mapFragment.getMap();
 
-                        try {
-                            double lat = Double.parseDouble(response.getString("lat"));
-                            double longit = Double.parseDouble(response.getString("long"));
-                            long validUntil = Long.parseLong(response.getString("valid_until"));
+                            try {
+                                long validUntil = Long.parseLong(response.getString("valid_until"));
+                                timeLeft = validUntil - (System.currentTimeMillis() / 1000);
+                                time_text.setText(timeLeftText());
 
-                            timeLeft = validUntil - (System.currentTimeMillis()/1000);
-                            Calendar deadline = Calendar.getInstance();
-                            deadline.setTimeInMillis(timeLeft*1000);
-                            String timeLeft_text = "Time left until Jerry moves: ";
-                            if (deadline.get(Calendar.MINUTE) > 0) {
-                                timeLeft_text += Integer.toString(deadline.get(Calendar.MINUTE)) + " minutes ";
+                                double new_latitude = Double.parseDouble(response.getString("lat"));
+                                double new_longitude = Double.parseDouble(response.getString("long"));
+
+                                if (latitude != new_latitude || longitude != new_longitude) {
+                                    latitude = new_latitude;
+                                    longitude = new_longitude;
+
+                                    map.clear();
+                                    map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Jerry"));
+                                }
+
+                                lat_text.setText("Latitude: " + Double.toString(latitude));
+                                long_text.setText("Longitude: " + Double.toString(longitude));
+                            } catch (JSONException e) {
+                                throwParseError();
                             }
-                            timeLeft_text += Integer.toString(deadline.get(Calendar.SECOND)) + " seconds.";
-
-                            lat_text.setText("Latitude: " + Double.toString(lat));
-                            long_text.setText("Longitude: " + Double.toString(longit));
-                            time_text.setText(timeLeft_text);
-
-                            map.addMarker(new MarkerOptions().position(new LatLng(lat, longit)).title("Jerry"));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
                         }
-
-                        stopThread = false;
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                final TextView lat_text = (TextView) findViewById(R.id.lat_text);
-                lat_text.setText("Error in parsing JSON.");
+                throwParseError();
             }
-        });
-// Add the request to the RequestQueue.
-        queue.add(jsonRequest);
+        }));
+    }
+
+    public void initiateScan(View view) {
+        IntentIntegrator integ = new IntentIntegrator(this);
+        integ.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String token = scanResult.getContents();
+            JSONObject params = new JSONObject();
+            try {
+                params.put("nim", "13510052");
+                params.put("token", token);
+            } catch (JSONException e) {
+                throwParseError();
+            }
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, CATCH_URL, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        if (catchResponseCode == 200) {
+                            showDialogBox("Token accepted.");
+                        } else if (catchResponseCode == 400) {
+                            showDialogBox("Missing parameter.");
+                        } else if (catchResponseCode == 403) {
+                            showDialogBox("Wrong parameter.");
+                        } else {
+                            showDialogBox("Unknown error.");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if (catchResponseCode == 400) {
+                        showDialogBox("Missing parameter.");
+                    } else if (catchResponseCode == 403) {
+                        showDialogBox("Wrong parameter.");
+                    } else {
+                        showDialogBox("Unknown error.");
+                    }
+                }
+            }){
+                @Override
+                protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                    catchResponseCode = response.statusCode;
+                    return super.parseNetworkResponse(response);
+                }
+            };
+            queue.add(request);
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         map.setMyLocationEnabled(true);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(POSISI_KOLAM_INTEL, 18.0f));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        queue = Volley.newRequestQueue(this);
+
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // TODO: gimana cara menghentikannya??
-                while (!stopThread) {
-                    try {
-                        Log.d(DEBUG_TAG, "Ext. thread running.");
-                        Thread.sleep(1000);
-                        if (timeLeft > -1) {
-                            timeLeft--;
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Calendar deadline = Calendar.getInstance();
-                                    deadline.setTimeInMillis(timeLeft*1000);
-                                    String timeLeft_text = "Time left until Jerry moves: ";
-                                    if (deadline.get(Calendar.MINUTE) > 0) {
-                                        timeLeft_text += Integer.toString(deadline.get(Calendar.MINUTE)) + " minutes ";
-                                    }
-                                    timeLeft_text += Integer.toString(deadline.get(Calendar.SECOND)) + " seconds.";
-                                    final TextView time_text = (TextView) findViewById(R.id.time_text);
-                                    time_text.setText(timeLeft_text);
-                                }
-                            });
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        askSpike();
+        ticker = new Thread(rticker);
+        ticker.start();
+        poller = new Thread(rpoller);
+        poller.start();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        stopThread = true;
-        Log.d(DEBUG_TAG, "Ext. thread stopped.");
+        ticker.interrupt();
+        poller.interrupt();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float degree = Math.round(event.values[0]);
+
+//        Log.d(DEBUG_TAG, Float.toString(degree));
+
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        // how long the animation will take place
+        ra.setDuration(210);
+
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // Start the animation
+        image.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not used
+    }
+
+    private String timeLeftText() {
+        Calendar deadline = Calendar.getInstance();
+        deadline.setTimeInMillis(timeLeft * 1000);
+        String timeLeft_text = "Time left until Jerry moves: ";
+        if (deadline.get(Calendar.MINUTE) > 0) {
+            timeLeft_text += Integer.toString(deadline.get(Calendar.MINUTE)) + " minutes ";
+        }
+        timeLeft_text += Integer.toString(deadline.get(Calendar.SECOND)) + " seconds.";
+        return timeLeft_text;
+    }
+
+    private void throwParseError() {
+        Toast.makeText(this, "JSON parse error.", Toast.LENGTH_LONG);
+    }
+
+    private void showDialogBox(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setMessage(message)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     @Override
