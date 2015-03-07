79bf3f291ac2a6d70aa0a94dd25aba89d625fd14
Mochamad Lutfi Fadlan <mochamadlutfifadlan@gmail.com>
2015-03-07 22:53:30 +0700

    First Commit. Be Nice.
    Core product

---
+++ TomJerry/app/src/main/java/com/example/adhikasigit/tomjerry/JsonParser.java
@@ -0,0 +1,116 @@
+package com.example.adhikasigit.tomjerry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Adhika Sigit on 3/7/2015.
+ */
+public class JsonParser {
+    final String TAG = "JsonParser.java";
+
+    static InputStream iStream = null;
+    static JSONObject jsonObject = null;
+    static String json = "";
+
+    public JSONObject getJSONFromUrl(String url) throws JSONException {
+
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = client.execute(httpGet);
+
+            HttpEntity httpEntity = response.getEntity();
+            iStream = httpEntity.getContent();
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Parse String to JSON object
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(iStream));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            iStream.close();
+            json = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        jsonObject = new JSONObject(json);
+
+        // return JSON Object
+        return jsonObject;
+
+    }
+
+    public static JSONObject postData(String token) throws JSONException {
+        String nim = "13512060";
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost("http://167.205.32.46/pbd/api/catch");
+
+        try {
+            List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
+            nameValuePair.add(new BasicNameValuePair("nim", nim));
+            nameValuePair.add(new BasicNameValuePair("token", token));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+            HttpResponse response = httpClient.execute(httpPost);
+
+            HttpEntity httpEntity = response.getEntity();
+            iStream = httpEntity.getContent();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Parse String to JSON object
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(iStream));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            iStream.close();
+            json = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        jsonObject = new JSONObject(json);
+
+        // return JSON Object
+        return jsonObject;
+
+    }
+}
